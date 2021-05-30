af455c41565407302963d106381b06a10adefcb3
ayankhann98@gmail.com <ayankhann98@gmail.com>
2021-05-30 20:42:41 +0530

    ADDING EDITtEXT WIDGET

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.widget.RelativeLayout;
 import android.widget.Button;
 import android.graphics.Color;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -24,10 +25,27 @@ public class MainActivity extends Activity {
         buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
         buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
         Button button=new Button(this);
-        //myLayout.setBackground(Color.parseColor("#000000"));
+        myLayout.setBackgroundColor(Color.MAGENTA);
         button.setBackgroundColor(Color.BLUE);
         button.setText("Click");
         myLayout.addView(button,buttonDetails);
+
+        EditText editText= new EditText(this);
+        button.setId(1);
+        editText.setId(2);
+        RelativeLayout.LayoutParams editTextDetails= new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        //editText.addRule(RelativeLayout.ABOVE,button.getId());
+        //editText.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        //Margin setting to editText
+        //editText.setMargins(0,0,0,50);
+
+        myLayout.addView(editText,editTextDetails);
+
+
         setContentView(myLayout);
     }
 }
